f1ef778d3cf3523f1b771add53c8ecffcd61c2a9
Vladyslav <vladbuldenko@gmail.com>
2024-08-13 22:36:22 +0300

    imported FE implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Root } from "./Root";
+import { ContextProvider } from "./store/store";
+import { ThemeProvider } from "./context/ThemeContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ContextProvider>
+      <ThemeProvider>
+        <Root />
+      </ThemeProvider>
+    </ContextProvider>
+  </StrictMode>
+);
